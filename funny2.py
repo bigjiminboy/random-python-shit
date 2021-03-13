4a4c5acf6f0b8742cf6bc203259b587962e51731
bigjiminboy <79030761+bigjiminboy@users.noreply.github.com>
2021-03-13 18:36:32 -0500

    Add files via upload
    original funny insult script in case you want it idk

---
+++ funny2.py
@@ -0,0 +1,24 @@
+name = input("What's your name fuckface?\n")
+import random
+x = random.randrange(1, 10)
+if x == 1:
+    x = ("Go fuck yourself")
+if x == 2:
+    x = ("You have no friends")
+if x == 3:
+    x = ("You\'re gonna die a virgin")
+if x == 4:
+    x = ("You\'re a real cunt")
+if x == 5:
+    x = ("No one loves you")
+if x == 6:
+    x = ("Shove a 12-foot steel pole up your ass")
+if x == 7:
+    x = ("I hope your house gets infested")
+if x == 8:
+    x = ("I have a trained sniper on your position")
+if x == 9:
+    x = ("You\'re a motel cumstain")
+if x == 10:
+    print("I hope you die")
+print(x, name)
